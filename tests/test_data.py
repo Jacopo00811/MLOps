883b33504ec831e870eaef727c11b5ffedfeb46c
Jacopo Ceccuti <jacopo.ceccuti@gmail.com>
2025-01-13 14:00:23 +0100

    Updated files

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,16 +11,16 @@ def test_my_dataset():
     dataset = MyDataset("data/raw")
     assert isinstance(dataset, Dataset)
 
-# @pytest.mark.skipif(not os.path.exists('MLOps/data/processed'), reason="Data files not found")
-# def test_data():
-#     train, test = corrupt_mnist()
-#     assert len(train) == 30000, "Dataset did not have the correct number of samples"
-#     assert len(test) == 5000, "Dataset did not have the correct number of samples"
-#     for dataset in [train, test]:
-#         for x, y in dataset:
-#             assert x.shape == (1, 28, 28)
-#             assert y in range(10)
-#     train_targets = torch.unique(train.tensors[1])
-#     assert (train_targets == torch.arange(0,10)).all()
-#     test_targets = torch.unique(test.tensors[1])
-#     assert (test_targets == torch.arange(0,10)).all()
+@pytest.mark.skipif(not os.path.exists('MLOps/data/processed'), reason="Data files not found")
+def test_data():
+    train, test = corrupt_mnist()
+    assert len(train) == 30000, "Dataset did not have the correct number of samples"
+    assert len(test) == 5000, "Dataset did not have the correct number of samples"
+    for dataset in [train, test]:
+        for x, y in dataset:
+            assert x.shape == (1, 28, 28)
+            assert y in range(10)
+    train_targets = torch.unique(train.tensors[1])
+    assert (train_targets == torch.arange(0,10)).all()
+    test_targets = torch.unique(test.tensors[1])
+    assert (test_targets == torch.arange(0,10)).all()
